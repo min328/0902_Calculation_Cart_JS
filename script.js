a392b0b865971921431488af4c9fe2cd00b3f0dd
min328 <kmj20212021@gmail.com>
2021-09-02 13:28:14 +0900

    currency type check

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
+var price = Number(document.getElementById("price").innerHTML);
+document.getElementById("price").innerHTML = price.toLocaleString();
+
 
 
 function fn_addCount(){
-    var price = document.getElementById("price").innerHTML;
     var productNum = Number(document.getElementById("productCount").value);
-    var result = 0;
+    var sum = 0;
     if(productNum>=0 && productNum <20){
         productNum++;
         document.getElementById("productCount").value = productNum;
-        document.getElementById("result").innerText = price * productNum;
+        sum = price * productNum;
+        sum = sum.toLocaleString();
+        document.getElementById("result").innerText = sum;
     } else {
         return false;
     }
 }
 
 function fn_subCount(){
-    var price = document.getElementById("price").innerHTML;
     var productNum = Number(document.getElementById("productCount").value);
-    var result = 0;
+    var sum = 0;
     if(productNum>0 && productNum<=20){
         productNum--;
         document.getElementById("productCount").value = productNum;
-        document.getElementById("result").innerText = price * productNum;
+        sum = price * productNum;
+        sum = sum.toLocaleString();
+        document.getElementById("result").innerText = sum;
     } else {
         return false;
     }
 }
 
 function fn_calResult(){
-    var price = document.getElementById("price").innerText;
     var productNum = Number(document.getElementById("productCount").value);
-    var result = 0;
+    var sum = 0;
     if(productNum>0 && productNum<=20){
-        result = price * productNum
-        document.getElementById("result").innerText = result;
+        sum = price * productNum
+        sum = sum.toLocaleString();
+        document.getElementById("result").innerText = sum;
         document.getElementById("productCount").value = productNum;
     } else if(productNum == null || productNum == ""){
         document.getElementById("productCount").value = productNum;
